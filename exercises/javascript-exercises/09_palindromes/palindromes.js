5001acbdedb5a4c44673e4258066fa094ef40a43
chrisbeltr <christianbeltran@uchicago.edu>
2024-08-22 22:34:24 -0500

    completed js exercises

--- exercises/javascript-exercises/09_palindromes/palindromes.js
+++ exercises/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    console.log(string);
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] != string[string.length - 1 - i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
--- exercises/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ exercises/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce(function(oldest, person) {
+        let personAge;
+        if (!person.yearOfDeath) {
+            let today = new Date();
+            today = today.getFullYear();
+            personAge = today - person.yearOfBirth;
+        } else personAge = person.yearOfDeath - person.yearOfBirth;
+        let oldestAge;
+        if (!oldest.yearOfDeath) {
+            let today = new Date();
+            today = today.getFullYear();
+            oldestAge = today - oldest.yearOfBirth;
+        } else oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        if (personAge > oldestAge) return person; else return oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
